731aff92897f85361bc8b09783fef5e1911e5e26
Kaasaibee <forcepunch8@gmail.com>
2026-02-20 15:28:42 -0500

    Commit

---
+++ app-files/data.js
@@ -0,0 +1,293 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-storm-brewing-station",
+      "name": "Storm-Brewing Station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6272134415236401,
+          "pitch": 0.3110239649864912,
+          "rotation": 0,
+          "target": "5-study-tables"
+        },
+        {
+          "yaw": 2.9409836447240263,
+          "pitch": 0.36769233079623653,
+          "rotation": 0,
+          "target": "1-sdi-f1-hallway"
+        },
+        {
+          "yaw": 0.7234405177687027,
+          "pitch": 0.19011839912247552,
+          "rotation": 0,
+          "target": "3-storms-grab-n-go"
+        },
+        {
+          "yaw": 0.2892147142910453,
+          "pitch": 0.12491859625846757,
+          "rotation": 0,
+          "target": "4-sdi-f1-mens-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sdi-f1-hallway",
+      "name": "SDI F1 Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.13052972872390356,
+          "pitch": 0.21952557681258256,
+          "rotation": 0,
+          "target": "0-storm-brewing-station"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-study-hallway",
+      "name": "Study Hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9781000466284961,
+          "pitch": 0.12305239985009386,
+          "rotation": 0.7853981633974483,
+          "target": "3-storms-grab-n-go"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-storms-grab-n-go",
+      "name": "Storm's Grab n Go",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.861437273868413,
+          "pitch": 0.2677727150470872,
+          "rotation": 0,
+          "target": "4-sdi-f1-mens-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-sdi-f1-mens-bathroom",
+      "name": "SDI F1 Men's Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8222126947387665,
+          "pitch": 0.28485562204804005,
+          "rotation": 0,
+          "target": "3-storms-grab-n-go"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-study-tables",
+      "name": "Study Tables",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.03600360756872,
+          "pitch": 0.3450018792490255,
+          "rotation": 0,
+          "target": "0-storm-brewing-station"
+        },
+        {
+          "yaw": 2.265607096183004,
+          "pitch": 0.2073034981860289,
+          "rotation": 4.71238898038469,
+          "target": "1-sdi-f1-hallway"
+        },
+        {
+          "yaw": -1.3647768900020267,
+          "pitch": 0.22435623379528025,
+          "rotation": 0,
+          "target": "3-storms-grab-n-go"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "RJ-360tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
